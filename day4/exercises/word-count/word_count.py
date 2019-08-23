6a5e5e612e81d3c53e3de4ec14a399cf9225c3d5
aliensmart <kaoua17@gmail.com>
2019-08-23 15:38:08 -0400

    Day5 update

--- day4/exercises/word-count/word_count.py
+++ day4/exercises/word-count/word_count.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 from collections import Counter
+# write a function count_words(filename)
+
+
 
 def count_words(filename):
     with open(filename) as f:
         file_name = f.read()
+        file_name = file_name.lower()
         list_words = file_name.split()
         couter = Counter(list_words)
 
